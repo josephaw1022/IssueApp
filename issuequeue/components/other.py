20719882eca77554870a7ebc62485f4833b072fa
josephaw1022 <josephsims1@gmail.com>
2021-06-21 13:25:15 -0400

    initial configuration for django project

---
+++ issuequeue/components/other.py
@@ -0,0 +1,25 @@
+DEBUG = True
+
+ALLOWED_HOSTS = ['josephaw1022.pythonanywhere.com', '*']
+
+
+ROOT_URLCONF = 'issuequeue.urls'
+
+
+WSGI_APPLICATION = 'issuequeue.wsgi.application'
+
+
+AUTH_PASSWORD_VALIDATORS = [
+    {
+        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
+    },
+]
---
+++ issuequeue/components/static.py
@@ -0,0 +1,11 @@
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/2.2/howto/static-files/
+
+STATIC_URL = '/static/'
+
+# default static files settings for PythonAnywhere.
+# see https://help.pythonanywhere.com/pages/DjangoStaticFiles for more info
+MEDIA_ROOT = '/home/josephaw1022/issuequeue/media'
+MEDIA_URL = '/media/'
+STATIC_ROOT = '/home/josephaw1022/issuequeue/static'
+STATIC_URL = '/static/'
